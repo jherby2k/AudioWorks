6f72e7a13992673b6345026ac01eaadb9de08db6
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-08-28 14:38:24 -0400

    0 should be a valid SampleCount.

--- AudioWorks/AudioWorks.Common/AudioInfo.cs
+++ AudioWorks/AudioWorks.Common/AudioInfo.cs
@@ -35,7 +35,7 @@ namespace AudioWorks.Common
                     "Value should be between 1 and 32.");
             if (sampleRate < 1)
                 throw new ArgumentOutOfRangeException(nameof(sampleRate), sampleRate, "Value should be 1 or greater.");
-            if (sampleCount <= 0)
+            if (sampleCount < 0)
                 throw new ArgumentOutOfRangeException(nameof(sampleCount), sampleCount, "Value cannot be negative.");
 
             Description = description;
