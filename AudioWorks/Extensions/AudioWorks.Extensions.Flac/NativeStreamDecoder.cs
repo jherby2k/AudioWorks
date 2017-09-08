ce09846e2d2f3b8d61e4223307996da876f9c681
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-09-08 09:39:56 -0400

    Rolling back simplification; need to maintain references to the callbacks for the lifetime of the decoder.

--- AudioWorks/Extensions/AudioWorks.Extensions.Flac/NativeStreamDecoder.cs
+++ AudioWorks/Extensions/AudioWorks.Extensions.Flac/NativeStreamDecoder.cs
@@ -7,56 +7,42 @@ namespace AudioWorks.Extensions.Flac
     class NativeStreamDecoder : IDisposable
     {
         [NotNull] readonly NativeStreamDecoderHandle _handle = SafeNativeMethods.StreamDecoderNew();
+        [NotNull] readonly NativeCallbacks.StreamDecoderReadCallback _readCallback;
+        [NotNull] readonly NativeCallbacks.StreamDecoderSeekCallback _seekCallback;
+        [NotNull] readonly NativeCallbacks.StreamDecoderTellCallback _tellCallback;
+        [NotNull] readonly NativeCallbacks.StreamDecoderLengthCallback _lengthCallback;
+        [NotNull] readonly NativeCallbacks.StreamDecoderEofCallback _eofCallback;
+        [NotNull] readonly NativeCallbacks.StreamDecoderWriteCallback _writeCallback;
+        [NotNull] readonly NativeCallbacks.StreamDecoderMetadataCallback _metadataCallback;
+        [NotNull] readonly NativeCallbacks.StreamDecoderErrorCallback _errorCallback;
         [NotNull] readonly Stream _stream;
 
-        internal NativeStreamDecoder([NotNull] FileStream stream)
+        internal NativeStreamDecoder([NotNull] Stream stream)
         {
+            // Need a reference to the callbacks for the lifetime of the decoder
+            _readCallback = ReadCallback;
+            _seekCallback = SeekCallback;
+            _tellCallback = TellCallback;
+            _lengthCallback = LengthCallback;
+            _eofCallback = EofCallback;
+            _writeCallback = WriteCallback;
+            _metadataCallback = MetadataCallback;
+            _errorCallback = ErrorCallback;
+
             _stream = stream;
         }
 
         internal void Initialize()
         {
             SafeNativeMethods.StreamDecoderInitializeStream(_handle,
-
-                // Read Callback
-                (IntPtr handle, byte[] buffer, ref int bytes, IntPtr userData) =>
-                {
-                    bytes = _stream.Read(buffer, 0, bytes);
-                    return bytes == 0 ? DecoderReadStatus.EndOfStream : DecoderReadStatus.Continue;
-                },
-
-                // Seek Callback
-                (handle, absoluteOffset, userData) =>
-                {
-                    _stream.Seek((long) absoluteOffset, SeekOrigin.Begin);
-                    return DecoderSeekStatus.Ok;
-                },
-
-                // Tell Callback
-                (IntPtr handle, out ulong absoluteOffset, IntPtr userData) =>
-                {
-                    absoluteOffset = (ulong) _stream.Position;
-                    return DecoderTellStatus.Ok;
-                },
-
-                // Length Callback
-                (IntPtr handle, out ulong streamLength, IntPtr userData) =>
-                {
-                    streamLength = (ulong) _stream.Length;
-                    return DecoderLengthStatus.Ok;
-                },
-
-                // EOF Callback
-                (handle, userData) => _stream.Position >= _stream.Length,
-
-                // Write Callback
-                (IntPtr handle, ref Frame frame, IntPtr buffer, IntPtr userData) => DecoderWriteStatus.Continue,
-
-                // Metadata Callback is virtual
-                MetadataCallback,
-
-                // Error Callback
-                (handle, error, userData) => { });
+                _readCallback,
+                _seekCallback,
+                _tellCallback,
+                _lengthCallback,
+                _eofCallback,
+                _writeCallback,
+                _metadataCallback,
+                _errorCallback);
         }
 
         internal bool ProcessMetadata()
@@ -80,7 +66,45 @@ namespace AudioWorks.Extensions.Flac
             _handle.Dispose();
         }
 
+        DecoderReadStatus ReadCallback(IntPtr handle, [NotNull] byte[] buffer, ref int bytes, IntPtr userData)
+        {
+            bytes = _stream.Read(buffer, 0, bytes);
+            return bytes == 0 ? DecoderReadStatus.EndOfStream : DecoderReadStatus.Continue;
+        }
+
+        DecoderSeekStatus SeekCallback(IntPtr handle, ulong absoluteOffset, IntPtr userData)
+        {
+            _stream.Seek((long) absoluteOffset, SeekOrigin.Begin);
+            return DecoderSeekStatus.Ok;
+        }
+
+        DecoderTellStatus TellCallback(IntPtr handle, out ulong absoluteOffset, IntPtr userData)
+        {
+            absoluteOffset = (ulong) _stream.Position;
+            return DecoderTellStatus.Ok;
+        }
+
+        DecoderLengthStatus LengthCallback(IntPtr handle, out ulong streamLength, IntPtr userData)
+        {
+            streamLength = (ulong) _stream.Length;
+            return DecoderLengthStatus.Ok;
+        }
+
+        bool EofCallback(IntPtr handle, IntPtr userData)
+        {
+            return _stream.Position >= _stream.Length;
+        }
+
+        static DecoderWriteStatus WriteCallback(IntPtr handle, ref Frame frame, IntPtr buffer, IntPtr userData)
+        {
+            return DecoderWriteStatus.Continue;
+        }
+
         protected virtual void MetadataCallback(IntPtr handle, IntPtr metadataBlock, IntPtr userData)
+        {
+        }
+
+        static void ErrorCallback(IntPtr handle, DecoderErrorStatus error, IntPtr userData)
         {
         }
     }
