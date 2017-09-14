3735049dec05f4221344bfd179c3018f138be5e4
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-09-13 23:28:10 -0400

    Implemented unit tests: "AudioMetadata throws an exception if the Title/Artist/Album is null".

---
+++ AudioWorks/AudioWorks.Api.Tests/AudioMetadataTests.cs
@@ -0,0 +1,42 @@
+﻿using AudioWorks.Common;
+using System;
+using Xunit;
+
+namespace AudioWorks.Api.Tests
+{
+    [Collection("Extensions")]
+    public sealed class AudioMetadataTests
+    {
+        [Fact(DisplayName = "AudioMetadata throws an exception if the Title is null")]
+        public void AudioMetadataTitleNullThrowsException()
+        {
+            // ReSharper disable once AssignNullToNotNullAttribute
+            Assert.Throws<ArgumentNullException>(() =>
+                new AudioMetadata{ Title = null });
+        }
+
+        [Fact(DisplayName = "AudioMetadata throws an exception if the Artist is null")]
+        public void AudioMetadataArtistNullThrowsException()
+        {
+            // ReSharper disable once AssignNullToNotNullAttribute
+            Assert.Throws<ArgumentNullException>(() =>
+                new AudioMetadata { Artist = null });
+        }
+
+        [Fact(DisplayName = "AudioMetadata throws an exception if the Album is null")]
+        public void AudioMetadataAlbumNullThrowsException()
+        {
+            // ReSharper disable once AssignNullToNotNullAttribute
+            Assert.Throws<ArgumentNullException>(() =>
+                new AudioMetadata { Album = null });
+        }
+
+        [Fact(DisplayName = "AudioMetadata throws an exception if the Comment is null")]
+        public void AudioMetadataCommentNullThrowsException()
+        {
+            // ReSharper disable once AssignNullToNotNullAttribute
+            Assert.Throws<ArgumentNullException>(() =>
+                new AudioMetadata { Comment = null });
+        }
+    }
+}
--- AudioWorks/AudioWorks.Common/AudioMetadata.cs
+++ AudioWorks/AudioWorks.Common/AudioMetadata.cs
@@ -1,6 +1,42 @@
-﻿namespace AudioWorks.Common
+﻿using JetBrains.Annotations;
+using System;
+
+namespace AudioWorks.Common
 {
+    [PublicAPI]
     public sealed class AudioMetadata
     {
+        [NotNull] string _title = string.Empty;
+        [NotNull] string _artist = string.Empty;
+        [NotNull] string _album = string.Empty;
+        [NotNull] string _comment = string.Empty;
+
+        [NotNull]
+        public string Title
+        {
+            get => _title;
+            set => _title = value ?? throw new ArgumentNullException();
+        }
+
+        [NotNull]
+        public string Artist
+        {
+            get => _artist;
+            set => _artist = value ?? throw new ArgumentNullException();
+        }
+
+        [NotNull]
+        public string Album
+        {
+            get => _album;
+            set => _album = value ?? throw new ArgumentNullException();
+        }
+
+        [NotNull]
+        public string Comment
+        {
+            get => _comment;
+            set => _comment = value ?? throw new ArgumentNullException();
+        }
     }
 }
