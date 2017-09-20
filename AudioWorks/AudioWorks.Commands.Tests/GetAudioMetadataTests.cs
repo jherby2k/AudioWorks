a00a04fef5f0bd6933cfcb171d40bd0e52cd396b
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-09-20 08:29:21 -0400

    Implemented unit test: "Get-AudioMetadata command exists".

---
+++ AudioWorks/AudioWorks.Commands.Tests/GetAudioMetadataTests.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Management.Automation;
+using Xunit;
+
+namespace AudioWorks.Commands.Tests
+{
+    [Collection("Module")]
+    public sealed class GetAudioMetadataTests
+    {
+        [NotNull] readonly ModuleFixture _moduleFixture;
+
+        public GetAudioMetadataTests(
+            [NotNull] ModuleFixture moduleFixture)
+        {
+            _moduleFixture = moduleFixture;
+        }
+
+        [Fact(DisplayName = "Get-AudioMetadata command exists")]
+        public void GetAudioMetadataCommandExists()
+        {
+            using (var ps = PowerShell.Create())
+            {
+                ps.Runspace = _moduleFixture.Runspace;
+                ps.AddCommand("Get-AudioMetadata");
+                try
+                {
+                    ps.Invoke();
+                }
+                catch (Exception e) when (!(e is CommandNotFoundException))
+                {
+                    // CommandNotFoundException is the only type we are testing for
+                }
+                Assert.True(true);
+            }
+        }
+    }
+}
---
+++ AudioWorks/AudioWorks.Commands/GetAudioMetadataCommand.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+using System.Management.Automation;
+
+namespace AudioWorks.Commands
+{
+    [PublicAPI]
+    [Cmdlet(VerbsCommon.Get, "AudioMetadata")]
+    public sealed class GetAudioMetadataCommand : Cmdlet
+    {
+    }
+}
