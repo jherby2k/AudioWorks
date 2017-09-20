b55c691b284a1e3b8ac59e4b877f22fdf6d62d6f
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-09-20 10:11:03 -0400

    Implemented unit test: "Set-AudioMetadata command exists".

---
+++ AudioWorks/AudioWorks.Commands.Tests/SetAudioMetadataTests.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Management.Automation;
+using Xunit;
+
+namespace AudioWorks.Commands.Tests
+{
+    [Collection("Module")]
+    public sealed class SetAudioMetadataTests
+    {
+        [NotNull] readonly ModuleFixture _moduleFixture;
+
+        public SetAudioMetadataTests(
+            [NotNull] ModuleFixture moduleFixture)
+        {
+            _moduleFixture = moduleFixture;
+        }
+
+        [Fact(DisplayName = "Set-AudioMetadata command exists")]
+        public void SetAudioMetadataCommandExists()
+        {
+            using (var ps = PowerShell.Create())
+            {
+                ps.Runspace = _moduleFixture.Runspace;
+                ps.AddCommand("Set-AudioMetadata");
+                try
+                {
+                    ps.Invoke();
+                }
+                catch (Exception e) when (!(e is CommandNotFoundException))
+                {
+                    // CommandNotFoundException is the only type we are testing for
+                }
+                Assert.True(true);
+            }
+        }
+    }
+}
---
+++ AudioWorks/AudioWorks.Commands/SetAudioMetadataCommand.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+using System.Management.Automation;
+
+namespace AudioWorks.Commands
+{
+    [PublicAPI]
+    [Cmdlet(VerbsCommon.Set, "AudioMetadata")]
+    public sealed class SetAudioMetadataCommand : Cmdlet
+    {
+    }
+}
