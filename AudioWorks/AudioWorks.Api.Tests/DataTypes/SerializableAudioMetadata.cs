5fa2c13307ed3e57f9320cd80bc5e16e45ac3ef5
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-10-03 16:24:03 -0400

    Simplified xunit "serialization".

--- AudioWorks/AudioWorks.Api.Tests/DataTypes/SerializableAudioMetadata.cs
+++ AudioWorks/AudioWorks.Api.Tests/DataTypes/SerializableAudioMetadata.cs
@@ -8,30 +8,14 @@ namespace AudioWorks.Api.Tests.DataTypes
     {
         public void Deserialize([NotNull] IXunitSerializationInfo info)
         {
-            Title = info.GetValue<string>("Title");
-            Artist = info.GetValue<string>("Artist");
-            Album = info.GetValue<string>("Album");
-            Genre = info.GetValue<string>("Genre");
-            Comment = info.GetValue<string>("Comment");
-            Day = info.GetValue<string>("Day");
-            Month = info.GetValue<string>("Month");
-            Year = info.GetValue<string>("Year");
-            TrackNumber = info.GetValue<string>("TrackNumber");
-            TrackCount = info.GetValue<string>("TrackCount");
+            foreach (var property in GetType().GetProperties())
+                property.SetValue(this, info.GetValue(property.Name, property.PropertyType));
         }
 
         public void Serialize([NotNull] IXunitSerializationInfo info)
         {
-            info.AddValue("Title", Title);
-            info.AddValue("Artist", Artist);
-            info.AddValue("Album", Album);
-            info.AddValue("Genre", Genre);
-            info.AddValue("Comment", Comment);
-            info.AddValue("Day", Day);
-            info.AddValue("Month", Month);
-            info.AddValue("Year", Year);
-            info.AddValue("TrackNumber", TrackNumber);
-            info.AddValue("TrackCount", TrackCount);
+            foreach (var property in GetType().GetProperties())
+                info.AddValue(property.Name, property.GetValue(this));
         }
     }
 }
