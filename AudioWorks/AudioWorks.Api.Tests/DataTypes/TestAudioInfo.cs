1b1022a6abff42e7cd432d01ce2fe6c6de763acf
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-10-16 14:18:17 -0400

    Simplified xunit "serialization".

--- AudioWorks/AudioWorks.Api.Tests/DataTypes/TestAudioInfo.cs
+++ AudioWorks/AudioWorks.Api.Tests/DataTypes/TestAudioInfo.cs
@@ -20,22 +20,14 @@ namespace AudioWorks.Api.Tests.DataTypes
 
         public void Deserialize([NotNull] IXunitSerializationInfo info)
         {
-            Description = info.GetValue<string>("Description");
-            Channels = info.GetValue<int>("Channels");
-            BitsPerSample = info.GetValue<int>("BitsPerSample");
-            SampleRate = info.GetValue<int>("SampleRate");
-            SampleCount = info.GetValue<long>("SampleCount");
-            BitRate = info.GetValue<int>("BitRate");
+            foreach (var property in GetType().GetProperties())
+                property.SetValue(this, info.GetValue(property.Name, property.PropertyType));
         }
 
         public void Serialize([NotNull] IXunitSerializationInfo info)
         {
-            info.AddValue("Description", Description);
-            info.AddValue("Channels", Channels);
-            info.AddValue("BitsPerSample", BitsPerSample);
-            info.AddValue("SampleRate", SampleRate);
-            info.AddValue("SampleCount", SampleCount);
-            info.AddValue("BitRate", BitRate);
+            foreach (var property in GetType().GetProperties())
+                info.AddValue(property.Name, property.GetValue(this));
         }
     }
 }
