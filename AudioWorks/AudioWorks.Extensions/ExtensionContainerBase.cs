7ddb5346e8632660698a0d0df3e508157554f325
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-10-17 22:35:26 -0400

    Resolve binding exception on netfx.

--- AudioWorks/AudioWorks.Extensions/ExtensionContainerBase.cs
+++ AudioWorks/AudioWorks.Extensions/ExtensionContainerBase.cs
@@ -3,6 +3,7 @@ using System.Composition.Hosting;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Runtime.Loader;
 
 namespace AudioWorks.Extensions
@@ -24,16 +25,26 @@ namespace AudioWorks.Extensions
         {
             // Search all extension directories for unresolved references
 
-            // .NET Core
+            // Assembly.LoadFrom only works on the full framework
+            if (RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework",
+                StringComparison.Ordinal))
+                AppDomain.CurrentDomain.AssemblyResolve += (context, name) =>
+                    Assembly.LoadFrom(_extensionRoot
+                        .EnumerateFiles($"{name.Name}.dll", SearchOption.AllDirectories)
+                        .FirstOrDefault()?
+                        .FullName);
+
+            // Use AssemblyLoadContext on .NET Core
+            else
+                UseAssemblyLoadContext();
+        }
+
+        static void UseAssemblyLoadContext()
+        {
+            // Workaround - this needs to reside in a separate method to avoid a binding exception with the desktop
+            // framework, as System.Runtime.Loader does not include a net462 library.
             AssemblyLoadContext.Default.Resolving += (context, name) =>
                 AssemblyLoadContext.Default.LoadFromAssemblyPath(_extensionRoot
-                    .EnumerateFiles($"{name.Name}.dll", SearchOption.AllDirectories)
-                    .FirstOrDefault()?
-                    .FullName);
-
-            // Full Framework
-            AppDomain.CurrentDomain.AssemblyResolve += (context, name) =>
-                Assembly.LoadFrom(_extensionRoot
                     .EnumerateFiles($"{name.Name}.dll", SearchOption.AllDirectories)
                     .FirstOrDefault()?
                     .FullName);
