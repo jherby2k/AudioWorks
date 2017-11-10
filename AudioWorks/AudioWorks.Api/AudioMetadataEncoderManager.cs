e310f0ce86dbe0736987513cc73c171f6b2048d0
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-11-10 10:43:26 -0500

    Doc cleanup.

--- AudioWorks/AudioWorks.Api/AudioMetadataEncoderManager.cs
+++ AudioWorks/AudioWorks.Api/AudioMetadataEncoderManager.cs
@@ -6,18 +6,18 @@ using System;
 namespace AudioWorks.Api
 {
     /// <summary>
-    /// Provides information about the available metadata encoders, which are used by <see cref="AudioFile"/>'s
-    /// SaveMetadata method.
+    /// Provides information about the available metadata encoders, which are used by an
+    /// <see cref="ITaggedAudioFile"/>'s SaveMetadata method.
     /// </summary>
     [PublicAPI]
     public static class AudioMetadataEncoderManager
     {
         /// <summary>
-        /// Gets information about the available settings that can be passed to <see cref="AudioFile"/>'s SaveMetadata
-        /// method, for a given file extension.
+        /// Gets information about the available settings that can be passed to an <see cref="ITaggedAudioFile"/>'s
+        /// SaveMetadata method, for a given file extension.
         /// </summary>
         /// <param name="extension">The file extension.</param>
-        /// <returns>Information about the available settings</returns>
+        /// <returns>Information about the available settings.</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="extension"/> is null.</exception>
         [NotNull]
         public static SettingInfoDictionary GetSettingInfo([NotNull] string extension)
