2e210b271493445ff5730ee1da8f274b57c9cd22
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-12-15 20:46:23 -0500

    Added required attribute.

---
+++ AudioWorks/AudioWorks.Extensions/AudioEncoderExportAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Composition;
+using JetBrains.Annotations;
+
+namespace AudioWorks.Extensions
+{
+    /// <summary>
+    /// Classes marked with this attribute will be loaded by AudioWorks when attempting to export an audio
+    /// file to another format.
+    /// </summary>
+    /// <remarks>
+    /// Classes marked with this attribute should implement <see cref="IAudioEncoder"/> and also be marked with
+    /// the <see cref="SharedAttribute"/> attribute.
+    /// </remarks>
+    /// <seealso cref="ExportAttribute"/>
+    [PublicAPI, MeansImplicitUse, BaseTypeRequired(typeof(IAudioEncoder))]
+    [MetadataAttribute, AttributeUsage(AttributeTargets.Class)]
+    public sealed class AudioEncoderExportAttribute : ExportAttribute
+    {
+        /// <summary>
+        /// Gets the name of the analyzer.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        [NotNull]
+        public string Name { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AudioEncoderExportAttribute"/> class.
+        /// </summary>
+        /// <param name="name">The name of the analyzer.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="name"/> is null or empty.</exception>
+        public AudioEncoderExportAttribute([NotNull] string name)
+            : base(typeof(IAudioMetadataEncoder))
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException("Value cannot be null or empty.", nameof(name));
+
+            Name = name;
+        }
+    }
+}
