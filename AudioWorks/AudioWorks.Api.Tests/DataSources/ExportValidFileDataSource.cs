e9a55ec35528da8c94a64eaa27ef0d1fbc1f158f
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-12-19 09:24:48 -0500

    FLAC and ALAC -> Wave test cases.

--- AudioWorks/AudioWorks.Api.Tests/DataSources/ExportValidFileDataSource.cs
+++ AudioWorks/AudioWorks.Api.Tests/DataSources/ExportValidFileDataSource.cs
@@ -42,6 +42,83 @@ namespace AudioWorks.Api.Tests.DataSources
                 "Wave",
                 null,
                 "D55BD1987676A7D6C2A04BF09C10F64F"
+            },
+            new object[]
+            {
+                "FLAC Level 5 8-bit 8000Hz Stereo.flac",
+                "Wave",
+                null,
+                "818EE6CBF16F76F923D33650E7A52708"
+            },
+            new object[]
+            {
+                "FLAC Level 5 16-bit 44100Hz Mono.flac",
+                "Wave",
+                null,
+                "509B83828F13945E4121E4C4897A8649"
+            },
+            new object[]
+            {
+                "FLAC Level 5 16-bit 44100Hz Stereo.flac",
+                "Wave",
+                null,
+                "5D4B869CD72BE208BC7B47F35E13BE9A"
+            },
+            new object[]
+            {
+                "FLAC Level 5 16-bit 48000Hz Stereo.flac",
+                "Wave",
+                null,
+                "EFBC44B9FA9C04449D67ECD16CB7F3D8"
+            },
+            new object[]
+            {
+                "FLAC Level 5 24-bit 96000Hz Stereo.flac",
+                "Wave",
+                null,
+                "D55BD1987676A7D6C2A04BF09C10F64F"
+            },
+            new object[]
+            {
+                "FLAC Level 5 16-bit 44100Hz Stereo (Tagged using defaults).flac",
+                "Wave",
+                null,
+                "5D4B869CD72BE208BC7B47F35E13BE9A"
+            },
+            new object[]
+            {
+                "ALAC 16-bit 44100Hz Mono.m4a",
+                "Wave",
+                null,
+                "509B83828F13945E4121E4C4897A8649"
+            },
+            new object[]
+            {
+                "ALAC 16-bit 44100Hz Stereo.m4a",
+                "Wave",
+                null,
+                "5D4B869CD72BE208BC7B47F35E13BE9A"
+            },
+            new object[]
+            {
+                "ALAC 16-bit 48000Hz Stereo.m4a",
+                "Wave",
+                null,
+                "EFBC44B9FA9C04449D67ECD16CB7F3D8"
+            },
+            new object[]
+            {
+                "ALAC 24-bit 96000Hz Stereo.m4a",
+                "Wave",
+                null,
+                "D55BD1987676A7D6C2A04BF09C10F64F"
+            },
+            new object[]
+            {
+                "ALAC 16-bit 44100Hz Stereo (Tagged).m4a",
+                "Wave",
+                null,
+                "5D4B869CD72BE208BC7B47F35E13BE9A"
             }
         };
 
