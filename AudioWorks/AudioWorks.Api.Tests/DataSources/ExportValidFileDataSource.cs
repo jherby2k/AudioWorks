6cb46a7ebad2ed488007beac6f16467f79a5eb4c
Jeremy Herbison <jeremy.herbison@gmail.com>
2017-12-19 09:18:35 -0500

    Additional Wave output tests.

--- AudioWorks/AudioWorks.Api.Tests/DataSources/ExportValidFileDataSource.cs
+++ AudioWorks/AudioWorks.Api.Tests/DataSources/ExportValidFileDataSource.cs
@@ -10,10 +10,38 @@ namespace AudioWorks.Api.Tests.DataSources
         {
             new object[]
             {
+                "LPCM 8-bit 8000Hz Stereo.wav",
+                "Wave",
+                null,
+                "818EE6CBF16F76F923D33650E7A52708"
+            },
+            new object[]
+            {
                 "LPCM 16-bit 44100Hz Mono.wav",
                 "Wave",
                 null,
                 "509B83828F13945E4121E4C4897A8649"
+            },
+            new object[]
+            {
+                "LPCM 16-bit 44100Hz Stereo.wav",
+                "Wave",
+                null,
+                "5D4B869CD72BE208BC7B47F35E13BE9A"
+            },
+            new object[]
+            {
+                "LPCM 16-bit 48000Hz Stereo.wav",
+                "Wave",
+                null,
+                "EFBC44B9FA9C04449D67ECD16CB7F3D8"
+            },
+            new object[]
+            {
+                "LPCM 24-bit 96000Hz Stereo.wav",
+                "Wave",
+                null,
+                "D55BD1987676A7D6C2A04BF09C10F64F"
             }
         };
 
