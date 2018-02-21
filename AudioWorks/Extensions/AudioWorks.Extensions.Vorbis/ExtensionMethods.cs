83f0a7d0a2b1e929fbfffd378083b2017354bae2
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-02-21 12:35:18 -0500

    Vorbis CoverArt writing (Vorbis writing in general is still broken, so tests are currently set to fail).

---
+++ AudioWorks/Extensions/AudioWorks.Extensions.Vorbis/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using JetBrains.Annotations;
+
+namespace AudioWorks.Extensions.Vorbis
+{
+    static class ExtensionMethods
+    {
+        public static void WriteBigEndian([NotNull] this BinaryWriter writer, uint value)
+        {
+            var buffer = BitConverter.GetBytes(value);
+            Array.Reverse(buffer);
+            writer.Write(buffer);
+        }
+    }
+}
