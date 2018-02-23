bb280f5163084a0215c5217ffab55b410a8d7db6
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-02-22 19:23:38 -0500

    Added generic TryGetValue (will simplify extension API).

--- AudioWorks/AudioWorks.Common/SettingDictionary.cs
+++ AudioWorks/AudioWorks.Common/SettingDictionary.cs
@@ -10,5 +10,23 @@ namespace AudioWorks.Common
     [PublicAPI]
     public class SettingDictionary : Dictionary<string, object>
     {
+        /// <summary>
+        /// Gets the value associated with the specified key and of the specified type.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>true, if the value is present in the dictionary. Otherwise, false.</returns>
+        public bool TryGetValue<TValue>([NotNull] string key, [CanBeNull] out TValue value)
+        {
+            if (TryGetValue(key, out var objectValue) && objectValue is TValue)
+            {
+                value = (TValue)objectValue;
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
     }
 }
