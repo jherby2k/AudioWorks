f9d7bbc8c12a889fc8f8b1ce360f7e8786fe667d
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-03-06 17:02:12 -0500

    CoverArt optimizations using nightly ImageSharp build. Some tests failing as JPEG results are slightly different. Ignoring until library stabilizes.

--- AudioWorks/AudioWorks.Common/CoverArt.cs
+++ AudioWorks/AudioWorks.Common/CoverArt.cs
@@ -33,44 +33,37 @@ namespace AudioWorks.Common
 
         internal CoverArt([NotNull] Stream stream)
         {
-            // TODO use Image.Identify in next release to get an IImageInfo instead
             var format = Image.DetectFormat(stream) ??
                          throw new ImageInvalidException("Not a valid image.");
 
-            using (var memoryStream = new MemoryStream())
-            {
-                try
+            var info = Image.Identify(stream);
+            Width = info.Width;
+            Height = info.Height;
+            ColorDepth = info.PixelType.BitsPerPixel;
+
+            stream.Position = 0;
+
+            // Bitmaps should be compressed in PNG format
+            if (format.Name.Equals("BMP", StringComparison.OrdinalIgnoreCase))
+                using (var memoryStream = new MemoryStream())
                 {
                     using (var image = Image.Load(stream))
-                    {
-                        Width = image.Width;
-                        Height = image.Height;
-
-                        if (format.Name.Equals("BMP", StringComparison.OrdinalIgnoreCase))
-                        {
-                            // Bitmaps should be converted to PNG format to save memory
-                            image.SaveAsPng(memoryStream);
-                            memoryStream.Position = 0;
-                            format = Image.DetectFormat(memoryStream);
-                        }
-
-                        // JPEG and PNG should be stored verbatim
-                        else
-                        {
-                            stream.Position = 0;
-                            stream.CopyTo(memoryStream);
-                        }
-                    }
+                        image.SaveAsPng(memoryStream);
+                    _data = memoryStream.ToArray();
+                    memoryStream.Position = 0;
+                    format = Image.DetectFormat(memoryStream);
                 }
-                catch (NotSupportedException)
+
+            // JPEG and PNG images should be stored verbatim
+            else if (stream is MemoryStream currentStream)
+                _data = currentStream.ToArray();
+            else
+                using (var memoryStream = new MemoryStream())
                 {
-                    throw new ImageInvalidException("Not a valid image.");
+                    stream.CopyTo(memoryStream);
+                    _data = memoryStream.ToArray();
                 }
 
-                _data = memoryStream.ToArray();
-            }
-
-            ColorDepth = 24; //TODO can read this from IImageInfo in next release
             Lossless = format.Name.Equals("PNG", StringComparison.OrdinalIgnoreCase);
             MimeType = format.DefaultMimeType;
             FileExtension = $".{format.FileExtensions.First()}";
