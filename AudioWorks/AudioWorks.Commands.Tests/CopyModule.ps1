0ac86303d845bfc4d6a87e48e60123c5ebb0e728
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-03-16 21:50:49 -0400

    Change module dependency to a file copy to avoid conflicting System.Management.Automation.dll.

---
+++ AudioWorks/AudioWorks.Commands.Tests/CopyModule.ps1
@@ -0,0 +1,23 @@
+﻿param(
+    [string] $Configuration,
+    [string] $ModuleProjectRoot,
+    [string] $ExtensionProjectRoot,
+    [string] $OutputRoot)
+
+"Copying module under $ModuleProjectRoot to $OutputRoot built using the $Configuration configuration."
+
+$projectOutputDir = $ModuleProjectRoot | Get-ChildItem -Filter bin | Get-ChildItem -Filter $Configuration | Get-ChildItem -Filter "netstandard2.0"
+Copy-Item -Path "$($projectOutputDir.FullName)\*.*" -Destination $OutputRoot
+
+"Copying all extensions under $ExtensionProjectRoot to $OutputRoot\Module\Extensions built using the $Configuration configuration."
+
+$outputDir = New-Item -Name Extensions -Path $OutputRoot -ItemType Directory -Force
+$excludeFiles = Get-Item -Path $OutputRoot | Get-ChildItem -File
+
+foreach ($projectDir in Get-ChildItem $ExtensionProjectRoot -Directory)
+{
+    $projectOutputDir = $projectDir | Get-ChildItem -Filter bin | Get-ChildItem -Filter $Configuration | Get-ChildItem -Filter "netstandard2.0"
+    $extensionName = $projectDir.Name -replace "AudioWorks.Extensions.", ""
+    Remove-Item -Path "$outputDir\$extensionName" -Recurse | Out-Null
+    Copy-Item -Path $projectOutputDir.FullName -Destination "$outputDir\$extensionName" -Recurse -Exclude $excludeFiles
+}
