c94b13ea5f5e3f0fe9a25114147012768915abd1
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-04-10 14:34:53 -0400

    Enhanced integer preservation tests.

--- AudioWorks/tests/AudioWorks.Extensions.Tests/SampleBufferTests.cs
+++ AudioWorks/tests/AudioWorks.Extensions.Tests/SampleBufferTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace AudioWorks.Extensions.Tests
@@ -23,18 +24,6 @@ namespace AudioWorks.Extensions.Tests
                 Assert.True(f >= -1f && f <= 1f));
         }
 
-        [Fact(DisplayName = "Maximum and minimum 16-bit integers are preserved")]
-        public void MaxAndMin16BitArePreserved()
-        {
-            var inSamples = new int[] { short.MinValue, short.MaxValue };
-            var outSamples = new int[inSamples.Length];
-
-            var buffer = new SampleBuffer(inSamples, 16);
-            buffer.CopyToInterleaved(outSamples, 16);
-
-            Assert.Equal(inSamples, outSamples);
-        }
-
         [Fact(DisplayName = "Maximum and minimum 32-bit integers stay in range")]
         public void MaxAndMin32BitStayInRange()
         {
@@ -48,14 +37,23 @@ namespace AudioWorks.Extensions.Tests
                 Assert.True(f >= -1f && f <= 1f));
         }
 
-        [Fact(DisplayName = "Maximum and minimum 32-bit integers are preserved")]
-        public void MaxAndMin32BitArePreserved()
+        [Theory(DisplayName = "Integers are preserved exactly")]
+        [InlineData(8, -0x80, 0x7F)]
+        [InlineData(16, -0x8000, 0x7FFF)]
+        [InlineData(24, -0x80_0000, 0x7F_FFFF)]
+        [InlineData(31, -0x40_0000, 0x3F_FFFF)]
+        public void IntegersArePreservedExactly(int bitsPerSample, int minValue, int maxValue)
         {
-            var inSamples = new[] { int.MinValue, int.MaxValue };
-            var outSamples = new int[inSamples.Length];
+            var inSamples = new int[4096];
+            for (int i = 1, j = minValue, k = maxValue; i < inSamples.Length; i += 2, j++, k--)
+            {
+                inSamples[i - 1] = Math.Min(j, maxValue);
+                inSamples[i] = Math.Max(k, minValue);
+            }
 
-            var buffer = new SampleBuffer(inSamples, 32);
-            buffer.CopyToInterleaved(outSamples, 32);
+            var outSamples = new int[inSamples.Length];
+            var buffer = new SampleBuffer(inSamples, 2, bitsPerSample);
+            buffer.CopyToInterleaved(outSamples, bitsPerSample);
 
             Assert.Equal(inSamples, outSamples);
         }
