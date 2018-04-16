88967e00e6760e22ad16c4e9d374693cf918eddc
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-04-16 14:48:00 -0400

    Added integer overflow test.

--- AudioWorks/tests/AudioWorks.Extensions.Tests/SampleBufferTests.cs
+++ AudioWorks/tests/AudioWorks.Extensions.Tests/SampleBufferTests.cs
@@ -11,6 +11,17 @@ namespace AudioWorks.Extensions.Tests
             Assert.Equal(0, SampleBuffer.Empty.Frames);
         }
 
+        [Fact(DisplayName = "32-bit integers don't overflow")]
+        public void IntegersDontOverflow()
+        {
+            var inSamples = new[] { int.MaxValue, int.MinValue };
+
+            var outSamples = new int[inSamples.Length];
+            new SampleBuffer(inSamples, 32).CopyToInterleaved(outSamples, 32);
+
+            Assert.True(true);
+        }
+
         [Theory(DisplayName = "Integers stay in range after conversion to float")]
         [InlineData(1, -1, 0)]
         [InlineData(2, -2, 1)]
