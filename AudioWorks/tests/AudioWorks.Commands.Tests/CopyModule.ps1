2b1b8c23f41cf81e7727ee75b4c9400f4cbdb7ad
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-06-15 23:21:36 -0400

    Copy the correct target assemblies.

--- AudioWorks/tests/AudioWorks.Commands.Tests/CopyModule.ps1
+++ AudioWorks/tests/AudioWorks.Commands.Tests/CopyModule.ps1
@@ -1,9 +1,10 @@
 ﻿param(
     [string] $Configuration,
+	[string] $Framework,
     [string] $ModuleProjectRoot,
     [string] $OutputRoot)
 
 "Copying module under $ModuleProjectRoot to $OutputRoot built using the $Configuration configuration."
 
-$projectOutputDir = $ModuleProjectRoot | Get-ChildItem -Filter bin | Get-ChildItem -Filter $Configuration | Get-ChildItem -Filter "netstandard2.0"
+$projectOutputDir = $ModuleProjectRoot | Get-ChildItem -Filter bin | Get-ChildItem -Filter $Configuration | Get-ChildItem -Filter $Framework
 Copy-Item -Path "$($projectOutputDir.FullName)\*.*" -Destination $OutputRoot
