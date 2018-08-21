372d26b7935f673c4acd85179239c95a3688f546
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-08-21 16:15:49 -0400

    Only run PublishModule.ps1 once against each framework.

--- AudioWorks/src/AudioWorks.Commands/PublishModule.ps1
+++ AudioWorks/src/AudioWorks.Commands/PublishModule.ps1
@@ -1,16 +1,17 @@
 ﻿param(
     [string] $ProjectDir,
-    [string] $Configuration)
+    [string] $Configuration,
+    [string] $Framework)
 
-$outputDir = "$($ProjectDir)bin\$Configuration\AudioWorks.Commands"
+$outputRoot = "$($ProjectDir)bin\$Configuration\AudioWorks.Commands"
+$outputDir = "$outputRoot\$Framework"
 
 "Clearing $outputDir..."
 
 if (Test-Path $outputDir) { Remove-Item -Path $outputDir -Recurse -ErrorAction Stop }
 
-"Publishing AudioWorks.Commands module to $outputDir."
+"Publishing $Framework PowerShell module to $outputDir."
 
-dotnet publish --no-build -c $Configuration -o "$outputDir\netstandard2.0" -f netstandard2.0
-dotnet publish --no-build -c $Configuration -o "$outputDir\netcoreapp2.1" -f netcoreapp2.1
-Move-Item -Path "$outputDir\netstandard2.0\*" -Destination $outputDir -Include "*.psd1", "*.ps1xml", "*.dll-Help.xml"
-Remove-Item -Path "$outputDir\*\*" -Recurse -Include "*.psd1", "*.ps1xml", "*.xml", "*.pdb", "*.deps.json"
+dotnet publish --no-build -c $Configuration -o "$outputDir" -f $Framework
+Copy-Item -Path "$outputDir\*" -Destination $outputRoot -Include "*.psd1", "*.ps1xml", "*.dll-Help.xml"
+Remove-Item -Path "$outputDir\*" -Recurse -Include "*.psd1", "*.ps1xml", "*.xml", "*.pdb", "*.deps.json"
