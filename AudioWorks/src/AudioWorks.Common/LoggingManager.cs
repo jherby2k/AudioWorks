a46be5df27e8d73d4fc9dd561a36f52b8d33ff2b
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-09-04 16:34:28 -0400

    Removed file-based logging (include abstraction only).

--- AudioWorks/src/AudioWorks.Common/LoggingManager.cs
+++ AudioWorks/src/AudioWorks.Common/LoggingManager.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Globalization;
-using System.IO;
 using JetBrains.Annotations;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using NLog.Config;
-using NLog.Extensions.Logging;
 
 namespace AudioWorks.Common
 {
@@ -20,42 +15,6 @@ namespace AudioWorks.Common
         /// <value>The logger factory.</value>
         [NotNull]
         [CLSCompliant(false)]
-        public static ILoggerFactory LoggerFactory { get; } = new LoggerFactory().AddNLog();
-
-        static LoggingManager()
-        {
-            if (!ConfigurationManager.Configuration.GetValue("FileLogging", true)) return;
-
-            var logFile = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                "AudioWorks",
-                "Log Files",
-                $"{DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss", CultureInfo.InvariantCulture)}.log");
-
-            // Log at "Info" level by default
-            // ReSharper disable once SwitchStatementMissingSomeCases
-            switch (ConfigurationManager.Configuration.GetValue("LogLevel", 2))
-            {
-                case 0:
-                    SimpleConfigurator.ConfigureForFileLogging(logFile, NLog.LogLevel.Trace);
-                    break;
-                case 1:
-                    SimpleConfigurator.ConfigureForFileLogging(logFile, NLog.LogLevel.Debug);
-                    break;
-                case 2:
-                    SimpleConfigurator.ConfigureForFileLogging(logFile, NLog.LogLevel.Info);
-                    break;
-                case 3:
-                    SimpleConfigurator.ConfigureForFileLogging(logFile, NLog.LogLevel.Warn);
-                    break;
-                case 4:
-                    SimpleConfigurator.ConfigureForFileLogging(logFile, NLog.LogLevel.Error);
-                    break;
-                case 5:
-                    SimpleConfigurator.ConfigureForFileLogging(logFile, NLog.LogLevel.Fatal);
-                    break;
-                // Don't log anything at 6+
-            }
-        }
+        public static ILoggerFactory LoggerFactory { get; } = new LoggerFactory();
     }
 }
