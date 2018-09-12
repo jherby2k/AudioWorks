675cf6dd7ff75a3bc7045d54dd6f74e3489ffedd
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-09-12 16:32:38 -0400

    Don't create too many assembly load handlers.

--- AudioWorks/src/AudioWorks.Commands/LoggingCmdlet.cs
+++ AudioWorks/src/AudioWorks.Commands/LoggingCmdlet.cs
@@ -6,6 +6,7 @@ using System.Management.Automation;
 #if !NETCOREAPP2_1
 using System.Reflection;
 using System.Runtime.InteropServices;
+using JetBrains.Annotations;
 #endif
 
 namespace AudioWorks.Commands
@@ -18,18 +19,19 @@ namespace AudioWorks.Commands
     {
         static LoggingCmdlet()
         {
+#if !NETCOREAPP2_1
+            // Workaround for binding issue under Windows PowerShell
+            if (RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework",
+                StringComparison.Ordinal))
+                AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolve;
+
+#endif
             CmdletLoggerProvider.Instance.Enable();
         }
 
         /// <inheritdoc/>
         protected override void BeginProcessing()
         {
-#if !NETCOREAPP2_1
-            if (RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework",
-                StringComparison.Ordinal))
-                ApplyRedirects();
-
-#endif
             Telemetry.TrackFirstLaunch();
         }
 
@@ -51,29 +53,26 @@ namespace AudioWorks.Commands
         }
 #if !NETCOREAPP2_1
 
-        static void ApplyRedirects()
+        [CanBeNull]
+        static Assembly AssemblyResolve(object sender, [NotNull] ResolveEventArgs args)
         {
-            // Workaround for binding issue under Windows PowerShell
-            AppDomain.CurrentDomain.AssemblyResolve += (context, args) =>
+            // Load the available version of Newtonsoft.Json, regardless of the requested version
+            if (args.Name.StartsWith("Newtonsoft.Json", StringComparison.Ordinal))
             {
-                // Load the available version of Newtonsoft.Json, regardless of the requested version
-                if (args.Name.StartsWith("Newtonsoft.Json", StringComparison.Ordinal))
-                {
-                    var jsonAssembly = Path.Combine(
-                        // ReSharper disable once AssignNullToNotNullAttribute
-                        Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Newtonsoft.Json.dll");
+                var jsonAssembly = Path.Combine(
+                    // ReSharper disable once AssignNullToNotNullAttribute
+                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Newtonsoft.Json.dll");
 
-                    if (!File.Exists(jsonAssembly)) return null;
+                if (!File.Exists(jsonAssembly)) return null;
 
-                    // Make sure the assembly is really Newtonsoft.Json
-                    var assemblyName = AssemblyName.GetAssemblyName(jsonAssembly);
-                    if (assemblyName.Name.Equals("Newtonsoft.Json", StringComparison.Ordinal) &&
-                        assemblyName.FullName.EndsWith("PublicKeyToken=30ad4fe6b2a6aeed", StringComparison.Ordinal))
-                        return Assembly.LoadFrom(jsonAssembly);
-                }
+                // Make sure the assembly is really Newtonsoft.Json
+                var assemblyName = AssemblyName.GetAssemblyName(jsonAssembly);
+                if (assemblyName.Name.Equals("Newtonsoft.Json", StringComparison.Ordinal) &&
+                    assemblyName.FullName.EndsWith("PublicKeyToken=30ad4fe6b2a6aeed", StringComparison.Ordinal))
+                    return Assembly.LoadFrom(jsonAssembly);
+            }
 
-                return null;
-            };
+            return null;
         }
 #endif
     }
