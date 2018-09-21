e4b1a5b72c4488538adbabe5bad227c0e08c16bf
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-09-21 19:57:49 -0400

    Add copyright / LGPL header to all source files.

--- AudioWorks/src/Extensions/AudioWorks.Extensions.Mp4/AssemblyInfo.cs
+++ AudioWorks/src/Extensions/AudioWorks.Extensions.Mp4/AssemblyInfo.cs
@@ -1,3 +1,18 @@
-﻿using System;
+﻿/* Copyright © 2018 Jeremy Herbison
+
+This file is part of AudioWorks.
+
+AudioWorks is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+AudioWorks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+details.
+
+You should have received a copy of the GNU Lesser General Public License along with AudioWorks. If not, see
+<https://www.gnu.org/licenses/>. */
+
+using System;
 
 [assembly: CLSCompliant(true)]
