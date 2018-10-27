15391f99ac84051fd38c006feb865b6dbc64932c
Jeremy Herbison <jeremy.herbison@gmail.com>
2018-10-26 23:11:07 -0400

    Added Get-AudioMetadataEncoderInfo cmdlet.

---
+++ AudioWorks/tests/AudioWorks.Commands.Tests/GetAudioMetadataEncoderInfoTests.cs
@@ -0,0 +1,82 @@
+﻿/* Copyright © 2018 Jeremy Herbison
+
+This file is part of AudioWorks.
+
+AudioWorks is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+AudioWorks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+details.
+
+You should have received a copy of the GNU Lesser General Public License along with AudioWorks. If not, see
+<https://www.gnu.org/licenses/>. */
+
+using System;
+using System.Management.Automation;
+using AudioWorks.Api;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace AudioWorks.Commands.Tests
+{
+    public sealed class GetAudioMetadataEncoderInfoTests : IClassFixture<ModuleFixture>
+    {
+        [NotNull] readonly ModuleFixture _moduleFixture;
+
+        public GetAudioMetadataEncoderInfoTests([NotNull] ModuleFixture moduleFixture)
+        {
+            _moduleFixture = moduleFixture;
+        }
+
+        [Fact(DisplayName = "Get-AudioMetadataEncoderInfo command exists")]
+        public void CommandExists()
+        {
+            using (var ps = PowerShell.Create())
+            {
+                ps.Runspace = _moduleFixture.Runspace;
+                ps.AddCommand("Get-AudioMetadataEncoderInfo");
+                try
+                {
+                    ps.Invoke();
+                }
+                catch (Exception e) when (!(e is CommandNotFoundException))
+                {
+                    // CommandNotFoundException is the only type we are testing for
+                }
+
+                Assert.True(true);
+            }
+        }
+
+        [Fact(DisplayName = "Get-AudioMetadataEncoderInfo has an OutputType of AudioMetadataEncoderInfo")]
+        public void OutputTypeIsAudioMetadataEncoderInfo()
+        {
+            using (var ps = PowerShell.Create())
+            {
+                ps.Runspace = _moduleFixture.Runspace;
+                ps.AddCommand("Get-Command")
+                    .AddArgument("Get-AudioMetadataEncoderInfo");
+                ps.AddCommand("Select-Object")
+                    .AddParameter("ExpandProperty", "OutputType");
+                ps.AddCommand("Select-Object")
+                    .AddParameter("ExpandProperty", "Type");
+
+                Assert.Equal(typeof(AudioMetadataEncoderInfo), (Type) ps.Invoke()[0].BaseObject);
+            }
+        }
+
+        [Fact(DisplayName = "Get-AudioMetadataEncoderInfo returns an AudioMetadataEncoderInfo")]
+        public void ReturnsAudioEncoderInfo()
+        {
+            using (var ps = PowerShell.Create())
+            {
+                ps.Runspace = _moduleFixture.Runspace;
+                ps.AddCommand("Get-AudioMetadataEncoderInfo");
+
+                Assert.IsAssignableFrom<AudioMetadataEncoderInfo>(ps.Invoke()[0].BaseObject);
+            }
+        }
+    }
+}
