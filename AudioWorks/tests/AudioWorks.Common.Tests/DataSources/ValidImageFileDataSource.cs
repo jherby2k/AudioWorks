1aa139fb966cd22b953f9c9bd8261313471414b2
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-01-08 15:44:30 -0500

    Preprocessor statement should be !NETFRAMEWORK, not !NETSTANDARD2_0.

--- AudioWorks/tests/AudioWorks.Common.Tests/DataSources/ValidImageFileDataSource.cs
+++ AudioWorks/tests/AudioWorks.Common.Tests/DataSources/ValidImageFileDataSource.cs
@@ -31,7 +31,7 @@ namespace AudioWorks.Common.Tests.DataSources
                 24,
                 true,
                 "image/png",
-#if WINDOWS && !NETSTANDARD2_0
+#if WINDOWS && !NETFRAMEWORK
                 "85E02F6C2BCF8112E16E63660CADFE02"
 #else
                 "C7B06AE783981771FA3806BBFF114EFF"
