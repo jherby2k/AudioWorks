ac5d9f0059e9b770432fa4d3c62f693007f1984b
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-01-14 14:15:52 -0500

    Changed osx* directories to official RID names.

--- AudioWorks/src/Extensions/AudioWorks.Extensions.ReplayGain/Ebur128LibHandler.cs
+++ AudioWorks/src/Extensions/AudioWorks.Extensions.ReplayGain/Ebur128LibHandler.cs
@@ -63,9 +63,9 @@ namespace AudioWorks.Extensions.ReplayGain
             var libPath = Path.Combine(
                 // ReSharper disable once AssignNullToNotNullAttribute
                 Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath),
-                osVersion.StartsWith("10.12", StringComparison.Ordinal) ? "osx.10.12" :
-                osVersion.StartsWith("10.13", StringComparison.Ordinal) ? "osx.10.13" :
-                "osx.10.14");
+                osVersion.StartsWith("10.12", StringComparison.Ordinal) ? "osx.10.12-x64" :
+                osVersion.StartsWith("10.13", StringComparison.Ordinal) ? "osx.10.13-x64" :
+                "osx.10.14-x64");
 
             AddUnmanagedLibraryPath(libPath);
 #else // LINUX
