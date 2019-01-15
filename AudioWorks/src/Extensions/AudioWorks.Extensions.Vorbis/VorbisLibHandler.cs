f8702aee49b7ee0de9d5658a5b0c0d4776c419b9
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-01-15 09:31:38 -0500

    Use local native assemblies preferentially on Ubuntu.

--- AudioWorks/src/Extensions/AudioWorks.Extensions.Vorbis/VorbisLibHandler.cs
+++ AudioWorks/src/Extensions/AudioWorks.Extensions.Vorbis/VorbisLibHandler.cs
@@ -18,13 +18,9 @@ using System;
 using System.Diagnostics;
 #endif
 using System.IO;
-#if !LINUX
 using System.Reflection;
-#endif
 using System.Runtime.InteropServices;
-#if !LINUX
 using System.Runtime.Loader;
-#endif
 using AudioWorks.Common;
 using AudioWorks.Extensibility;
 using JetBrains.Annotations;
@@ -65,28 +61,23 @@ namespace AudioWorks.Extensions.Vorbis
                 osVersion.StartsWith("10.13", StringComparison.Ordinal) ? "osx.10.13-x64" :
                 "osx.10.14-x64"));
 #else // LINUX
-            if (!VerifyLibrary("libvorbis.so.0"))
-            {
-                logger.LogWarning(
-                    GetDistribution().Equals("Ubuntu", StringComparison.OrdinalIgnoreCase)
-                        ? "Missing libvorbis.so.0. Run 'sudo apt-get install -y libvorbis0a && sudo updatedb' then restart AudioWorks."
-                        : "Missing libvorbis.so.0.");
-                return false;
-            }
+            var release = GetRelease();
 
-            if (!VerifyLibrary("libvorbisenc.so.2"))
-            {
-                logger.LogWarning(
-                    GetDistribution().Equals("Ubuntu", StringComparison.OrdinalIgnoreCase)
-                        ? "Missing libvorbisenc.so.2. Run 'sudo apt-get install -y libvorbisenc2 && sudo updatedb' then restart AudioWorks."
-                        : "Missing libvorbisenc.so.2.");
-                return false;
-            }
+            if (release.StartsWith("Ubuntu 16.04", StringComparison.OrdinalIgnoreCase))
+                AddUnmanagedLibraryPath(Path.Combine(
+                    // ReSharper disable once AssignNullToNotNullAttribute
+                    Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath),
+                    "ubuntu.16.04-x64"));
+            else if (release.StartsWith("Ubuntu 18.04", StringComparison.OrdinalIgnoreCase))
+                AddUnmanagedLibraryPath(Path.Combine(
+                    // ReSharper disable once AssignNullToNotNullAttribute
+                    Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath),
+                    "ubuntu.18.04-x64"));
 
             if (!VerifyLibrary("libogg.so.0"))
             {
                 logger.LogWarning(
-                    GetDistribution().Equals("Ubuntu", StringComparison.OrdinalIgnoreCase)
+                    release.StartsWith("Ubuntu", StringComparison.OrdinalIgnoreCase)
                         ? "Missing libogg.so.0. Run 'sudo apt-get install -y libogg0 && sudo updatedb' then restart AudioWorks."
                         : "Missing libogg.so.0.");
                 return false;
@@ -99,33 +90,21 @@ namespace AudioWorks.Extensions.Vorbis
             return true;
         }
 
-#if LINUX
-        [Pure]
-        static bool VerifyLibrary([NotNull] string libraryName)
+        static void AddUnmanagedLibraryPath([NotNull] string libPath)
         {
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo("locate", $"-r {libraryName}$")
-                {
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                }
-            };
-            process.Start();
-            process.StandardOutput.ReadToEnd();
-            process.WaitForExit();
-            return process.ExitCode == 0;
+            ((ExtensionLoadContext)AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly()))
+                .AddUnmanagedLibraryPath(libPath);
         }
+#if LINUX
 
         [NotNull]
-        public static string GetDistribution()
+        public static string GetRelease()
         {
             try
             {
                 var process = new Process
                 {
-                    StartInfo = new ProcessStartInfo("lsb_release", "-i -s")
+                    StartInfo = new ProcessStartInfo("lsb_release", "-d -s")
                     {
                         RedirectStandardOutput = true,
                         UseShellExecute = false,
@@ -143,11 +122,23 @@ namespace AudioWorks.Extensions.Vorbis
                 return string.Empty;
             }
         }
-#else
-        static void AddUnmanagedLibraryPath([NotNull] string libPath)
+
+        [Pure]
+        static bool VerifyLibrary([NotNull] string libraryName)
         {
-            ((ExtensionLoadContext) AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly()))
-                .AddUnmanagedLibraryPath(libPath);
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo("locate", $"-r {libraryName}$")
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            process.Start();
+            process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return process.ExitCode == 0;
         }
 #endif
 #if OSX
