60cb37af87b71b9272650a9495173b1e2fee9214
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-01-25 09:25:58 -0500

    Replace LGPL with AGPL preamble.

--- AudioWorks/src/Extensions/AudioWorks.Extensions.Opus/NativeEnumerations.cs
+++ AudioWorks/src/Extensions/AudioWorks.Extensions.Opus/NativeEnumerations.cs
@@ -2,15 +2,15 @@
 
 This file is part of AudioWorks.
 
-AudioWorks is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+AudioWorks is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
 License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
 version.
 
 AudioWorks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
 details.
 
-You should have received a copy of the GNU Lesser General Public License along with AudioWorks. If not, see
+You should have received a copy of the GNU Affero General Public License along with AudioWorks. If not, see
 <https://www.gnu.org/licenses/>. */
 
 namespace AudioWorks.Extensions.Opus
