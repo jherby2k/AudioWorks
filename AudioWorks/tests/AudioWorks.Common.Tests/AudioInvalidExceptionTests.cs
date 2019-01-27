e9b47705ffb0ba8980449ec0e423fb44552bdaff
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-01-27 11:01:41 -0500

    Remove unnecessary using statements.

--- AudioWorks/tests/AudioWorks.Common.Tests/AudioInvalidExceptionTests.cs
+++ AudioWorks/tests/AudioWorks.Common.Tests/AudioInvalidExceptionTests.cs
@@ -13,8 +13,6 @@ details.
 You should have received a copy of the GNU Affero General Public License along with AudioWorks. If not, see
 <https://www.gnu.org/licenses/>. */
 
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 using AudioWorks.TestUtilities;
 using JetBrains.Annotations;
 using Xunit;
