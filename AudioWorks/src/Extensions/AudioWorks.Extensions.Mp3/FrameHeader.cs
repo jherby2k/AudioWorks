644933fbc30ff412d050e499b519a6cb04029da1
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-04-02 13:38:53 -0400

    Fix regression in MPEG version parsing.

--- AudioWorks/src/Extensions/AudioWorks.Extensions.Mp3/FrameHeader.cs
+++ AudioWorks/src/Extensions/AudioWorks.Extensions.Mp3/FrameHeader.cs
@@ -139,6 +139,7 @@ namespace AudioWorks.Extensions.Mp3
 
         enum MpegVersion
         {
+            [UsedImplicitly] TwoPointFive = 0b00000000,
             Two = 0b00000010,
             One = 0b00000011
         }
