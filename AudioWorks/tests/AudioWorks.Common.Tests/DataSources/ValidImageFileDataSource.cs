eca359f21b5bf9b91f66244767116bd6d710071f
Jeremy Herbison <jeremy.herbison@gmail.com>
2020-04-30 21:14:19 -0600

    Updated ImageSharp to RC1.

--- AudioWorks/tests/AudioWorks.Common.Tests/DataSources/ValidImageFileDataSource.cs
+++ AudioWorks/tests/AudioWorks.Common.Tests/DataSources/ValidImageFileDataSource.cs
@@ -30,13 +30,7 @@ namespace AudioWorks.Common.Tests.DataSources
                 24,
                 true,
                 "image/png",
-#if WINDOWS && NETCOREAPP2_1
-                "85E02F6C2BCF8112E16E63660CADFE02"
-#elif WINDOWS && NETCOREAPP
-                "368F0A80FDB2080365F923D9CD9BBE5F"
-#else
-                "C7B06AE783981771FA3806BBFF114EFF"
-#endif
+                "9315650A78F4292A5527586198C4F3C8"
             },
 
             new object[]
