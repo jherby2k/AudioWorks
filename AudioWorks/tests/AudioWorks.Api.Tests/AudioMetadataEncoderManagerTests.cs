10aa1bc7d3db8e5e7f638d0eff80494a11c07165
Jeremy Herbison <jeremy.herbison@gmail.com>
2020-07-17 22:05:34 -0600

    Added test cases

---
+++ AudioWorks/tests/AudioWorks.Api.Tests/AudioMetadataEncoderManagerTests.cs
@@ -0,0 +1,41 @@
+﻿/* Copyright © 2020 Jeremy Herbison
+
+This file is part of AudioWorks.
+
+AudioWorks is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+AudioWorks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along with AudioWorks. If not, see
+<https://www.gnu.org/licenses/>. */
+
+using System;
+using AudioWorks.Api.Tests.DataSources;
+using AudioWorks.Common;
+using AudioWorks.TestUtilities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AudioWorks.Api.Tests
+{
+    public sealed class AudioMetadataEncoderManagerTests
+    {
+        public AudioMetadataEncoderManagerTests(ITestOutputHelper outputHelper) =>
+            LoggerManager.AddSingletonProvider(() => new XunitLoggerProvider()).OutputHelper = outputHelper;
+
+        [Theory(DisplayName = "AudioMetadataEncoderManager's GetEncoderInfo method returns the audio metadata encoders")]
+        [MemberData(nameof(AudioMetadataEncoderInfoDataSource.Data), MemberType = typeof(AudioMetadataEncoderInfoDataSource))]
+        public void GetFormatInfoReturnsAudioFileFormats(
+            string expectedExtension,
+            string expectedFormat,
+            string expectedDescription) =>
+            Assert.Contains(AudioMetadataEncoderManager.GetEncoderInfo(), info =>
+                expectedExtension.Equals(info.Extension, StringComparison.OrdinalIgnoreCase) &&
+                expectedFormat.Equals(info.Format, StringComparison.OrdinalIgnoreCase) &&
+                expectedDescription.Equals(info.Description, StringComparison.OrdinalIgnoreCase));
+    }
+}
---
+++ AudioWorks/tests/AudioWorks.Api.Tests/DataSources/AudioMetadataEncoderInfoDataSource.cs
@@ -0,0 +1,58 @@
+﻿/* Copyright © 2020 Jeremy Herbison
+
+This file is part of AudioWorks.
+
+AudioWorks is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+AudioWorks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along with AudioWorks. If not, see
+<https://www.gnu.org/licenses/>. */
+
+using System.Collections.Generic;
+
+namespace AudioWorks.Api.Tests.DataSources
+{
+    public static class AudioMetadataEncoderInfoDataSource
+    {
+        static readonly List<object[]> _data = new List<object[]>
+        {
+            new object[]
+            {
+                ".flac",
+                "FLAC",
+                "FLAC"
+            },
+            new object[]
+            {
+                ".m4a",
+                "iTunes",
+                "iTunes-compatible MPEG-4"
+            },
+            new object[]
+            {
+                ".mp3",
+                "ID3",
+                "ID3 version 2.x"
+            },
+            new object[]
+            {
+                ".ogg",
+                "Vorbis",
+                "Vorbis Comments"
+            },
+            new object[]
+            {
+                ".opus",
+                "Opus",
+                "Opus Comments"
+            }
+        };
+
+        public static IEnumerable<object[]> Data => _data;
+    }
+}
