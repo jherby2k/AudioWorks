182c06a7c2abcedfe76d836f5924c49d1a2a05a3
Jeremy Herbison <jeremy.herbison@gmail.com>
2020-11-10 20:56:40 -0700

    Revert accidental change

--- AudioWorks/src/Extensions/AudioWorks.Extensions.Mp4/ItunesAudioMetadataDecoder.cs
+++ AudioWorks/src/Extensions/AudioWorks.Extensions.Mp4/ItunesAudioMetadataDecoder.cs
@@ -28,7 +28,6 @@ namespace AudioWorks.Extensions.Mp4
 
         public AudioMetadata ReadMetadata(Stream stream)
         {
-            using var reader = new BinaryReader(stream);
             var mp4 = new Mp4Model(stream);
             if (mp4.DescendToAtom("moov", "udta", "meta", "ilst"))
                 return new IlstAtomToMetadataAdapter(mp4, mp4.GetChildAtomInfo());
