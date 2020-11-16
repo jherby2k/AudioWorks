75532352a298a65064515dc543dfed6066b3f635
Jeremy Herbison <jeremy.herbison@gmail.com>
2020-11-16 16:40:20 -0700

    Added scripts for quickly switching repositories

---
+++ AudioWorks/src/Extensions/UseLocalRepository.ps1
@@ -0,0 +1,21 @@
+﻿<# Copyright © 2020 Jeremy Herbison
+
+This file is part of AudioWorks.
+
+AudioWorks is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+AudioWorks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along with AudioWorks. If not, see
+<https://www.gnu.org/licenses/>. #>
+
+$localFeedDir = Join-Path -Path $([System.Environment]::GetFolderPath(28)) -ChildPath AudioWorks | Join-Path -ChildPath LocalFeed
+$settingsFile = Join-Path -Path $([System.Environment]::GetFolderPath(26)) -ChildPath AudioWorks | Join-Path -ChildPath settings.json
+
+$content = Get-Content $settingsFile | ConvertFrom-Json
+$content.ExtensionRepository = $localFeedDir
+$content | ConvertTo-Json | Set-Content -Path $settingsFile
---
+++ AudioWorks/src/Extensions/UseRemoteRepository.ps1
@@ -0,0 +1,21 @@
+﻿<# Copyright © 2020 Jeremy Herbison
+
+This file is part of AudioWorks.
+
+AudioWorks is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+AudioWorks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along with AudioWorks. If not, see
+<https://www.gnu.org/licenses/>. #>
+
+$remoteFeed = 'https://www.myget.org/F/audioworks-extensions-v5/api/v3/index.json'
+$settingsFile = Join-Path -Path $([System.Environment]::GetFolderPath(26)) -ChildPath AudioWorks | Join-Path -ChildPath settings.json
+
+$content = Get-Content $settingsFile | ConvertFrom-Json
+$content.ExtensionRepository = $remoteFeed
+$content | ConvertTo-Json | Set-Content -Path $settingsFile
