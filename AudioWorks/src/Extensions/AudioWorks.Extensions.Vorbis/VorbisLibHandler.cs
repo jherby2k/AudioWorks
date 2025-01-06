e17efe1f91870d838381bb8ad26a5f8233f10e60
Jeremy Herbison <jeremy.herbison@gmail.com>
2025-01-06 10:32:14 -0700

    Updated binaries for Mac

--- AudioWorks/src/Extensions/AudioWorks.Extensions.Vorbis/VorbisLibHandler.cs
+++ AudioWorks/src/Extensions/AudioWorks.Extensions.Vorbis/VorbisLibHandler.cs
@@ -50,17 +50,9 @@ namespace AudioWorks.Extensions.Vorbis
             AddUnmanagedLibraryPath(libPath);
 #endif
 #elif OSX
-            var osVersion = GetOSVersion();
             AddUnmanagedLibraryPath(Path.Combine(
                 Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().Location).LocalPath)!,
-                osVersion.StartsWith("10.15", StringComparison.Ordinal)
-                    ? "macos.10.15-x64" :
-                osVersion.StartsWith("11", StringComparison.Ordinal) ? RuntimeInformation.ProcessArchitecture == Architecture.Arm64
-                    ? "macos.11-arm64"
-                    : "macos.11-x64" :
-                RuntimeInformation.ProcessArchitecture == Architecture.Arm64
-                    ? "macos.12-arm64"
-                    : "macos.12-x64"));
+                $"macos.{GetOSVersion()}-{GetArch()}"));
 #else // LINUX
             var release = GetRelease();
             if (release.StartsWith("Ubuntu", StringComparison.OrdinalIgnoreCase))
@@ -139,9 +131,11 @@ namespace AudioWorks.Extensions.Vorbis
                 process.Start();
                 var result = process.StandardOutput.ReadToEnd();
                 process.WaitForExit();
-                return result.Trim();
+                return result.Trim()[..2];
             }
         }
+
+        static string GetArch() => RuntimeInformation.ProcessArchitecture == Architecture.Arm64 ? "arm64" : "x64";
 #endif
     }
 }
