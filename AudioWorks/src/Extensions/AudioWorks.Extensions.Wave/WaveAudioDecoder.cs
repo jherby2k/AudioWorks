07d926c521059315acc932912f8b453962ffd0e2
Jeremy Herbison <jeremy.herbison@gmail.com>
2025-03-10 09:26:08 -0600

    Removed unnecessary using statement

--- AudioWorks/src/Extensions/AudioWorks.Extensions.Wave/WaveAudioDecoder.cs
+++ AudioWorks/src/Extensions/AudioWorks.Extensions.Wave/WaveAudioDecoder.cs
@@ -16,7 +16,6 @@ You should have received a copy of the GNU Affero General Public License along w
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Runtime.InteropServices;
 using AudioWorks.Common;
 using AudioWorks.Extensibility;
 
