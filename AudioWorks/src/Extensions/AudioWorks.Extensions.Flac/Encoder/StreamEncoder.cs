d5dc4ee4e28b5eec44d17d6f98e4b0365c9de50b
Jeremy Herbison <jeremy.herbison@gmail.com>
2025-03-21 11:54:26 -0600

    Remove unnecessary using

--- AudioWorks/src/Extensions/AudioWorks.Extensions.Flac/Encoder/StreamEncoder.cs
+++ AudioWorks/src/Extensions/AudioWorks.Extensions.Flac/Encoder/StreamEncoder.cs
@@ -17,7 +17,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.IO.Pipes;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
